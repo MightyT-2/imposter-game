6e48bafe80702360926e42eb1a53b8027310528d
Mighty T-2 <timmightyt2@gmail.com>
2026-01-30 08:04:49 -0600

    Add more code

--- Imposter.py
+++ Imposter.py
@@ -7,6 +7,7 @@ inputCount    = ''
 dictList      = list(x[:-4] for x in listdir('dictionaries') if x[-4:] == '.txt')
 index         = 0
 indexSelector = ''
+userIndex     = 0
 
 class player:
     name = ''
@@ -17,6 +18,7 @@ class player:
 
 if __name__ == "__main__":
 
+    # Get the player list
     inputPlayers = ''
     while inputPlayers != '!done':
         inputPlayers = input('Enter player name: ')
@@ -24,6 +26,7 @@ if __name__ == "__main__":
             playerList.append(player(inputPlayers.strip()))
     system('clear')
 
+    # Get the imposter count
     while imposterCount <= 0 or imposterCount > len(playerList) / 2:
         try:
             imposterCount = int(input(f'How many imposters do you want (player count: {len(playerList)}): '))
@@ -35,11 +38,17 @@ if __name__ == "__main__":
             print('Not a valid number!')
     system('clear')
 
+    # Get the category
     index         = 0
-    indexSelector = 0
+    indexSelector = ''
+    userIndex     = -1
     print('Select a category.')
     for x in dictList:
-        print(f' {index}) {x}')
+        print(f" {index}) {x} ({len(open(x + '.txt', 'r').read().split('\n'))})")
         index += 1
-    while indexSelector <= 0 or indexSelector >= index:
-        index = input(' >> ')
+    while userIndex <= -1 or userIndex >= index:
+        indexSelector = input(' >> ')
+        try:
+            userIndex = int(indexSelector)
+        except:
+            userIndex = -1
