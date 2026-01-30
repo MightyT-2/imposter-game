3b6e5e44877c3ae2fb82859968e43aa1d2e722f9
Mighty T-2 <timmightyt2@gmail.com>
2026-01-30 08:56:03 -0600

    Add More Code/Make Clearing the Screen Cross-Platform

--- Imposter.py
+++ Imposter.py
@@ -1,4 +1,5 @@
 from os import system, listdir
+from platform import system as os
 
 inputPlayers  = ''
 playerList    = []
@@ -8,13 +9,35 @@ dictList      = list(x[:-4] for x in listdir('dictionaries') if x[-4:] == '.txt'
 index         = 0
 indexSelector = ''
 userIndex     = 0
+currentFile   = ''
+wordCount     = 0
 
-class player:
-    name = ''
-    isImposter = False
-    score = 0
+class Player:
+    playerCount = 0
+
     def __init__(this, playerName):
         this.name = playerName
+        this.isImposter = False
+        this.score = 0
+        this.playerId = Player.playerCount
+        Player.playerCount += 1
+    
+    def setImposter(this):
+        this.isImposter = True
+        return
+
+    def reset(this):
+        this.isImposter = False
+        return
+    
+    def getId(this):
+        return this.playerId
+
+def clearScreen():
+    if os() == 'Windows':
+        system('cls')
+    else:
+        system('clear')
 
 if __name__ == "__main__":
 
@@ -23,8 +46,9 @@ if __name__ == "__main__":
     while inputPlayers != '!done':
         inputPlayers = input('Enter player name: ')
         if inputPlayers != '!done':
-            playerList.append(player(inputPlayers.strip()))
-    system('clear')
+            playerList.append(Player(inputPlayers.strip()))
+            print(playerList[len((playerList)) - 1].getId())
+    clearScreen()
 
     # Get the imposter count
     while imposterCount <= 0 or imposterCount > len(playerList) / 2:
@@ -36,7 +60,7 @@ if __name__ == "__main__":
                 print('You need less than half the player count as imposter!')
         except:
             print('Not a valid number!')
-    system('clear')
+    clearScreen()
 
     # Get the category
     index         = 0
@@ -44,7 +68,8 @@ if __name__ == "__main__":
     userIndex     = -1
     print('Select a category.')
     for x in dictList:
-        print(f" {index}) {x} ({len(open(x + '.txt', 'r').read().split('\n'))})")
+        wordCount = len(open('dictionaries\\' + x + '.txt', 'r').read().split('\n'))
+        print(f" {index}) {x} ({wordCount})")
         index += 1
     while userIndex <= -1 or userIndex >= index:
         indexSelector = input(' >> ')
@@ -52,3 +77,7 @@ if __name__ == "__main__":
             userIndex = int(indexSelector)
         except:
             userIndex = -1
+    currentFile = dictList[userIndex]
+    clearScreen()
+
+    #
